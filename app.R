68a15519148dc9f1430c6b08340054aef1cebbc5
jfunction <1636190+jfunction@users.noreply.github.com>
2024-06-30 09:21:41 +0200

    Move to single-page model run and reduce the user input parameters to just `cov1`, `cov2` and `pt`.

--- app.R
+++ app.R
@@ -60,77 +60,34 @@ ui <- page_navbar(
         )
       ),
       nav_panel(
-        title = "Parameters",
-        layout_column_wrap(
-          class = "parameters",
-          width = 1 / 3,
-          heights_equal = "row",
-          max_height = 400,
-          gap = 100,
-          sliderInput(inputId = "beta", label = "Probability of transmission", value = 10, min = 0, max = 100, post = "%"),
-          sliderInput(inputId = "cov1", label = "Vaccine 1 coverage", value = 65, min = 0, max = 100, post = "%"),
-          sliderInput(inputId = "cov2", label = "Vaccine 2 coverage", value = 50, min = 0, max = 100, post = "%"),
-          sliderInput(inputId = "pt", label = "Probability of seeking treatment", value = 100, min = 0, max = 100, post = "%"),
-          numericInput(inputId = "rs", label = "Incubation rate", value = 1, min = 1, max = 10),
-          numericInput(inputId = "rr", label = "Natural recovery rate", value = 1, min = 1, max = 10),
-          sliderInput(inputId = "ve1", label = "Vaccine efficacy", value = 100, min = 0, max = 100, post = "%"),
-          numericInput(inputId = "rt", label = "Treatment seeking rate", value = 1, min = 1, max = 10),
-          numericInput(inputId = "rtr", label = "Treatment recovery rate", value = 1, min = 1, max = 10),
-          actionButton(inputId = "runModel", label = "Run Model", class = "btn btn-success")
-        )
-      ),
-      nav_panel(
-        title = "Scenarios",
-        h4(icon("cubes"), "Build a Package of Interventions"),
-        fluidRow(
-          column(
-            width = 6,
-            source("www/R/ui/intervention_first_vaccine.R", local = TRUE)$value,
-          ),
-          column(
-            width = 6,
-            source("www/R/ui/intervention_second_vaccine.R", local = TRUE)$value
-          )
-        ),
-        # Floating box to name and run a package of interventions
-        div(
-          class = "float-go-interv",
-          div(
-            class = "card",
-            div(id = "intervention_card_header", class = "card-header", "Simulation of the Package"),
-            div(
-              id = "intervention_card_body", class = "card-body",
-              layout_columns(
-                span(
-                  "Package Name:",
-                  textInput("package_name", label = NULL, placeholder = "Vaccine 1", width = "250px")
-                ),
-                actionButton("go_interventions", "Run", class = "btn btn-success"),
-                col_widths = c(9, 3)
-              )
-            )
-          )
-        )
-      ),
-      nav_panel(
-        title = "Results",
+        title = "Run Model",
         accordion(
           open = TRUE,
-          accordion_panel(
-            title = "Incidence by age group",
-            height = "100%",
-            card(
-              full_screen = TRUE,
-              plotOutput(outputId = "model_plot_incidence")
-            )
-          ),
-          accordion_panel(
-            title = "Treatment by age group",
-            height = "100%",
-            card(
-              full_screen = TRUE,
-              plotOutput(outputId = "model_plot_treatment")
-            )
+          fluidRow(
+            column(3, fluidRow(
+              column(12,
+                     # sliderInput(inputId = "beta", label = "Probability of transmission", value = 10, min = 0, max = 100, post = "%"),
+                     sliderInput(inputId = "cov1", label = "Vaccine 1 coverage", value = 65, min = 0, max = 100, post = "%"),
+                     sliderInput(inputId = "cov2", label = "Vaccine 2 coverage", value = 50, min = 0, max = 100, post = "%"),
+                     sliderInput(inputId = "pt", label = "Probability of seeking treatment", value = 100, min = 0, max = 100, post = "%"),
+                     # numericInput(inputId = "rs", label = "Incubation rate", value = 1, min = 1, max = 10),
+                     # numericInput(inputId = "rr", label = "Natural recovery rate", value = 1, min = 1, max = 10),
+                     # sliderInput(inputId = "ve1", label = "Vaccine efficacy", value = 100, min = 0, max = 100, post = "%"),
+                     # numericInput(inputId = "rt", label = "Treatment seeking rate", value = 1, min = 1, max = 10),
+                     # numericInput(inputId = "rtr", label = "Treatment recovery rate", value = 1, min = 1, max = 10),
+                     actionButton(inputId = "runModel", label = "Run Model", class = "btn btn-success")
+               )
+            )),
+            column(12-3, fluidRow(
+              card(
+                full_screen = TRUE,
+                plotOutput(outputId = "model_plot_incidence")
+              ),
+              card(
+                full_screen = TRUE,
+                plotOutput(outputId = "model_plot_treatment")
+              )
+            ))
           )
         )
       )
@@ -169,15 +126,17 @@ contact <- makeContactMatrix(
 # define the server function
 server <- function(input, output, session) {
   params <- reactive({
-    makeParameters(beta = input$beta/100,
+    makeParameters(
+                   # beta = input$beta/100,
                    cov1 = input$cov1/100,
                    cov2 = input$cov2/100,
-                   pt = input$pt/100,
-                   rs = input$rs,
-                   rr = input$rr,
-                   ve1 = input$ve1/100,
-                   rt = input$rt,
-                   rtr = input$rtr)
+                   pt = input$pt/100
+                   # rs = input$rs,
+                   # rr = input$rr,
+                   # ve1 = input$ve1/100,
+                   # rt = input$rt,
+                   # rtr = input$rtr
+                   )
   })
 
   modelOutput <- reactiveVal()
